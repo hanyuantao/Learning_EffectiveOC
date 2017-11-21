e92d4f54549a4578b8153b092d4fba9053fb8e4a
oncer <545574484@qq.com>
2017-11-21 22:46:44 +0800

    方法 6 、7

---
+++ EffectiveOC/EffectiveOC/OCPersonObject.h
@@ -0,0 +1,13 @@
+//
+//  OCPersonObject.h
+//  EffectiveOC
+//
+//  Created by An on 2017/11/21.
+//  Copyright © 2017年 once. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OCPersonObject : NSObject
+
+@end
---
+++ EffectiveOC/EffectiveOC/OCPersonObject.m
@@ -0,0 +1,13 @@
+//
+//  OCPersonObject.m
+//  EffectiveOC
+//
+//  Created by An on 2017/11/21.
+//  Copyright © 2017年 once. All rights reserved.
+//
+
+#import "OCPersonObject.h"
+
+@implementation OCPersonObject
+
+@end
