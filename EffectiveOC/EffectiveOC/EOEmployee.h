8df1c00ca8fb781938f498a2b14c1b17ade525c3
oncer <545574484@qq.com>
2017-11-22 23:39:51 +0800

    方法8-9

---
+++ EffectiveOC/EffectiveOC/EOEmployee.h
@@ -0,0 +1,13 @@
+//
+//  EOEmployee.h
+//  EffectiveOC
+//
+//  Created by An on 2017/11/22.
+//  Copyright © 2017年 once. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EOEmployee : NSObject
+
+@end
---
+++ EffectiveOC/EffectiveOC/EOEmployee.m
@@ -0,0 +1,13 @@
+//
+//  EOEmployee.m
+//  EffectiveOC
+//
+//  Created by An on 2017/11/22.
+//  Copyright © 2017年 once. All rights reserved.
+//
+
+#import "EOEmployee.h"
+
+@implementation EOEmployee
+
+@end
